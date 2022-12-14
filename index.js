b51986358f572bd6182573f13a85ba596ad592a9
unknown <nguyenngocha18062002@gmail.com>
2022-12-15 01:07:05 +0700

    add morgan lib

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-app.get('/tin-tuc', (req, res) => {
+app.use(morgan('combined'))
+
+app.get('/home', (req, res) => {
   res.send('Hello World!')
 })
 
